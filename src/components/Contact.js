d31349249c83efa05c2d040a09db78f8eb1d5248
PatelDev14 <devp1400@gmail.com>
2024-12-11 23:00:30 -0500

    Got the client directory removed

---
+++ src/components/Contact.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Contact = () => (
+  <section className="contact" id="contact">
+    <div className="contact-container">
+      <div className="contact-image">
+        <img src="/contact.jpg" alt="Contact Us" />
+        <div className="contact-text-overlay">
+          <h2>Contact Us</h2>
+          <p>Email: parthcob42@gmail.com</p>
+          <p>Phone: +1 905-781-8935</p>
+          <div className="social-icons">
+            <a href="https://www.facebook.com" aria-label="Facebook"><i className="fab fa-facebook"></i></a>
+            <a href="https://www.instagram.com" aria-label="Instagram"><i className="fab fa-instagram"></i></a>
+            <a href="https://www.x.com" aria-label="Twitter"><i className="fab fa-twitter"></i></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+);
+
+export default Contact;
