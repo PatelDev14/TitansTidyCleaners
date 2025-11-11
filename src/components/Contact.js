3885ac25582203c9b3658fc5c1b31d53ecfa02a4
Dev Patel <dev@mac.home>
2025-11-11 09:47:52 -0500

    Change in the Contacts

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,8 +7,8 @@ const Contact = () => (
         <img src="/contact.jpg" alt="Contact Us" />
         <div className="contact-text-overlay">
           <h2>Contact Us</h2>
-          <p>Email: titanstidycleaners@gmail.com</p>
-          <p>Phone: +1 905-781-8935</p>
+          <p>Email: 14devpatel00@gmail.com</p>
+          <p>Phone: +1 437-221-2087</p>
           <div className="social-icons">
             <a href="https://www.facebook.com" aria-label="Facebook"><i className="fab fa-facebook"></i></a>
             <a href="https://www.instagram.com" aria-label="Instagram"><i className="fab fa-instagram"></i></a>
